84b8b894010bfd626476f28198ae204b2c8b6ac3
Jacob Williams <jacobaw@gmail.com>
2024-07-05 18:45:49 -0700

    Connect to Anthropic

--- main.ts
+++ main.ts
@@ -1,81 +1,107 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, RequestUrlParam, RequestUrlResponse, Setting, requestUrl } from 'obsidian';
+import { Anthropic, ClientOptions } from '@anthropic-ai/sdk';
+import { TextBlock } from '@anthropic-ai/sdk/resources';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface AutoImageAltSettings {
+	anthropicApiKey: string;
+	anthropicModel: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AutoImageAltSettings = {
+	anthropicApiKey: '',
+	anthropicModel: 'claude-3-5-sonnet-20240620',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+type SensitiveSettings = Pick<AutoImageAltSettings, 'anthropicApiKey'>
+function sensitiveSettings({ anthropicApiKey }: AutoImageAltSettings): SensitiveSettings {
+	return { anthropicApiKey };
+}
+
+type NonsensitiveSettings = Pick<AutoImageAltSettings, 'anthropicModel'>
+function nonsensitiveSettings({ anthropicModel }: AutoImageAltSettings): NonsensitiveSettings {
+	return { anthropicModel };
+}
+
+class AltGen {
+	anthropic: Anthropic;
+	settings: AutoImageAltSettings;
+
+	constructor(settings: AutoImageAltSettings) {
+		this.settings = settings;
+		const opts: ClientOptions = {
+			fetch: async (url: RequestInfo, init?: RequestInit): Promise<Response> => {
+				const fetchReq = (typeof url === 'string' || url instanceof String) ? null : (url as Request);
+				const urlString = fetchReq === null ? (url as string) : fetchReq.url;
+				const obsidianReq: RequestUrlParam = {
+					url: urlString,
+				};
+
+				if (fetchReq?.body) {
+					obsidianReq.body = fetchReq.body;
+				} else if (init?.body) {
+					obsidianReq.body = init.body;
+				}
+
+				if (fetchReq?.headers) {
+					obsidianReq.headers = {...fetchReq.headers};
+				} else if (init?.headers) {
+					obsidianReq.headers = {...init.headers};
+				}
+				if (obsidianReq.headers) {
+					// SimpleURLLoaderWrapper will throw ERR_INVALID_ARGUMENT if you try to pass this
+					delete obsidianReq.headers['content-length'];
+				}
+
+				if (fetchReq?.method) {
+					obsidianReq.method = fetchReq.method;
+				} else if (init?.method) {
+					obsidianReq.method = init.method;
+				}
+
+				const obsidianResp: RequestUrlResponse = await requestUrl(obsidianReq);
+				const fetchResp = new Response(
+					obsidianResp.arrayBuffer,
+					{
+						status: obsidianResp.status,
+						headers: obsidianResp.headers,
+					});
+				return fetchResp;
+			},
+		};
+		if (settings.anthropicApiKey) {
+			opts.apiKey = settings.anthropicApiKey;
+		}
+		this.anthropic = new Anthropic(opts);
+	}
+
+	async generate(): Promise<string> {
+		const message = await this.anthropic.messages.create({
+			max_tokens: 1024,
+			messages: [{ role: 'user', content: 'Tell me a one-line joke.' }],
+			model: this.settings.anthropicModel,
+		});
+
+		return message.content.filter(c => c.type == "text").map(c => (c as TextBlock).text).join("\n\n");
+	}
+}
+
+export default class AutoImageAlt extends Plugin {
+	settings: AutoImageAltSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new AutoImageAltSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "generate-selected",
+			name: "Generate alt-text for selected image",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const altgen = new AltGen(this.settings);
+				const result = await altgen.generate();
+				new Notice(result);
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -87,30 +113,21 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(nonsensitiveSettings(this.settings));
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+function fragmentForHTML(html: string): DocumentFragment {
+	const fragment = document.createDocumentFragment();
+	const div = fragment.createDiv();
+	div.innerHTML = html;
+	return fragment;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class AutoImageAltSettingTab extends PluginSettingTab {
+	plugin: AutoImageAlt;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoImageAlt) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +138,24 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Anthropic API key')
+			.setDesc(fragmentForHTML('Your API key. This is used to make requests to Claude containing your images and asking it to describe them. See <a href="https://docs.anthropic.com/en/api/getting-started">the Anthropic API\'s Getting Started page</a>.<br/><b>Note:</b> For security reasons, this is currently not saved, and must be re-entered after restarting Obsidian or reloading the plugin.'))
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.anthropicApiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.anthropicApiKey = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('Anthropic model')
+			.setDesc(fragmentForHTML('The model identifier you want to use when making requests to Claude. See <a href="https://docs.anthropic.com/en/docs/about-claude/models">the Anthropic User Guide\'s Models page</a>.'))
+			.addText(text => text
+				.setPlaceholder('Enter a model identifier')
+				.setValue(this.plugin.settings.anthropicModel)
+				.onChange(async (value) => {
+					this.plugin.settings.anthropicModel = value;
 					await this.plugin.saveSettings();
 				}));
 	}
